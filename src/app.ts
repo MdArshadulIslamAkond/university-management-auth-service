713ade6fe831fb4f03357f528c429f976f986e5e
Arshadul-Islam <arshadul36@gmail.com>
2025-01-04 17:13:03 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 const app: Application = express()
 // const port = 3000
 
 // middleware
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+// parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //Testing
 app.get('/', (req: Request, res: Response) => {
   res.send('Working Successfully!')
 })
 
-export default app;
+export default app
+
+//package json
+// "lint:fix": "eslint --fix",
